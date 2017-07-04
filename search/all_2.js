8b1f80ccfabe56def19b3e6cf4d039cd4047d26b
Mauro Bellone <bellonemauro@gmail.com>
2017-07-04 22:48:20 +0200

    update documentation

--- search/all_2.js
+++ search/all_2.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['decodechannelline',['decodeChannelLine',['../classfluicell_1_1_p_p_c1api.html#a52a365e6edb33d79bb32799715b6d3ae',1,'fluicell::PPC1api']]],
   ['decodedataline',['decodeDataLine',['../classfluicell_1_1_p_p_c1api.html#aef71416237c9d9e7f421e5d59b794896',1,'fluicell::PPC1api']]],
-  ['decreasedropletsize',['decreaseDropletSize',['../classfluicell_1_1_p_p_c1api.html#a5ff696a084280d0c26a7a02337983dbd',1,'fluicell::PPC1api']]],
-  ['decreaseflowspeed',['decreaseFlowspeed',['../classfluicell_1_1_p_p_c1api.html#a9ae29e3ff67e66e002c1c5b02e3ab824',1,'fluicell::PPC1api']]],
-  ['decreasevacuum5p',['decreaseVacuum5p',['../classfluicell_1_1_p_p_c1api.html#a1fd2058da69745247bdd5b2c65da33f0',1,'fluicell::PPC1api']]],
+  ['decreasedropletsize',['decreaseDropletSize',['../classfluicell_1_1_p_p_c1api.html#ad7750724b22ddabfaf370b6670c6f134',1,'fluicell::PPC1api']]],
+  ['decreaseflowspeed',['decreaseFlowspeed',['../classfluicell_1_1_p_p_c1api.html#ada2244db1d03ec03c8c24b67f6ff5216',1,'fluicell::PPC1api']]],
+  ['decreasevacuum',['decreaseVacuum',['../classfluicell_1_1_p_p_c1api.html#a21f447ab5faee10a91edcc71c64d6b14',1,'fluicell::PPC1api']]],
   ['description',['description',['../structfluicell_1_1_p_p_c1api_1_1serial_device_info.html#aa5a87b8bdf647b193cbc838cec3f3e06',1,'fluicell::PPC1api::serialDeviceInfo']]],
   ['disconnectcom',['disconnectCOM',['../classfluicell_1_1_p_p_c1api.html#a29aa8a41e818a7700094d3ee6ddb4572',1,'fluicell::PPC1api']]],
   ['duration',['Duration',['../structfluicell_1_1_p_p_c1api_1_1command.html#ac650823606e10b7026cfcb6f75886a43',1,'fluicell::PPC1api::command']]]
--- search/all_7.js
+++ search/all_7.js
@@ -1,9 +1,9 @@
 var searchData=
 [
   ['i',['i',['../structfluicell_1_1_p_p_c1api_1_1_p_p_c1__data.html#a8f059a8108e02d4e3c6c62cecf342aff',1,'fluicell::PPC1api::PPC1_data']]],
-  ['increasedropletsize',['increaseDropletSize',['../classfluicell_1_1_p_p_c1api.html#ad6908613ee9e4d2d02ff7d98b75b6d71',1,'fluicell::PPC1api']]],
-  ['increaseflowspeed',['increaseFlowspeed',['../classfluicell_1_1_p_p_c1api.html#ac06372063cd2d396dfbf8ef74b15b59c',1,'fluicell::PPC1api']]],
-  ['increasevacuum5p',['increaseVacuum5p',['../classfluicell_1_1_p_p_c1api.html#af87c04323f33794950bea585e490268d',1,'fluicell::PPC1api']]],
+  ['increasedropletsize',['increaseDropletSize',['../classfluicell_1_1_p_p_c1api.html#a8dc0fa2362307ad46124e69d60f82b5e',1,'fluicell::PPC1api']]],
+  ['increaseflowspeed',['increaseFlowspeed',['../classfluicell_1_1_p_p_c1api.html#a918fa76ec0db05957f70bcdf70a08b08',1,'fluicell::PPC1api']]],
+  ['increasevacuum',['increaseVacuum',['../classfluicell_1_1_p_p_c1api.html#a99df5ecdc3004af7fae97644316aa3a8',1,'fluicell::PPC1api']]],
   ['isconnected',['isConnected',['../classfluicell_1_1_p_p_c1api.html#a6568854933586fc0847edbded368fc0d',1,'fluicell::PPC1api']]],
   ['isrunning',['isRunning',['../classfluicell_1_1_p_p_c1api.html#ac08f5db982c3c13334042cd7f679ee73',1,'fluicell::PPC1api']]]
 ];
